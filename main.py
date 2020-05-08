14879ec393b3f9644b067d8e454210cfc34c1d5a
Miraculin <colton.cheung@hotmail.ca>
2020-05-07 21:22:59 -0700

    added command line parameters and handling

--- main.py
+++ main.py
@@ -2,14 +2,41 @@ import printer
 import textFilter
 from scoring import scoreAbsouluteWordDifference as errorFun
 import time
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Starts colemak trainer')
+
+    parser.add_argument('-n', dest='numWords', type=int,
+                        default=10, help='number of words to type')
+
+    parser.add_argument('-d', metavar='difficulty', type=int,
+                        default=0, dest='difficulty',
+                        help='Word length settings:\n 0 - 0-4 letters \n 1 - 5-7 letters \n 2 - 8+ letters \n 10 - 0-7 letters \n 210 - any length')
+
+    parser.add_argument('--homerow', dest='homerow', action='store_true',
+                        help='Use home row letters only')
+
+    args = parser.parse_args()
+
     printer.printColemakLayout()
-    numWords = 10
-    wordsToType = textFilter.generateNWordsByLength(numWords, 0, True)
+
+    numWords = args.numWords
+    difficulty = args.difficulty
+    homerow = args.homerow
+
+    print(numWords)
+    print(difficulty)
+    print(homerow)
+    wordsToType = textFilter.generateNWordsByLength(numWords, difficulty, homerow)
     displayText = " ".join(wordsToType)
-    t = time.time()
+
+    for i in range(0, 3):
+        print(str(3-i))
+        time.sleep(1)
+
     print(displayText)
+    t = time.time()
     typed = input()
     print("Errors: " + str(errorFun(displayText, typed)))
     timeTaken = time.time()-t
